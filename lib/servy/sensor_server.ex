b19adf2ca7e121c2c799821aa45850b4a5ffebc0
Joaquin Ipar <jipar@hexacta.com>
2022-04-05 23:51:06 -0300

    Exercise: Change refresh_interval dynamically.

--- lib/servy/sensor_server.ex
+++ lib/servy/sensor_server.ex
@@ -4,29 +4,38 @@ defmodule Servy.SensorServer do
 
   use GenServer
 
+  defmodule State do
+    defstruct state: %{}, refresh_interval: :timer.seconds(60)
+  end
+
   # Client Interface
 
   def start do
-    GenServer.start(__MODULE__, %{}, name: @name)
+    GenServer.start(__MODULE__, %State{}, name: @name)
   end
 
   def get_sensor_data do
     GenServer.call @name, :get_sensor_data
   end
 
-  # Server Callbacks
-
-  def init(_state) do
-    initial_state = run_tasks_to_get_sensor_data()
-    schedule_refresh(5)
-    {:ok, initial_state}
+  def set_refresh_interval(time) do
+    GenServer.cast @name, {:set_refresh_interval, time}
   end
 
-  def handle_info(:refresh, _state) do
+
+  # Server Callbacks
+
+  def init(%State{refresh_interval: interval} = state) do
+    initial_state = run_tasks_to_get_sensor_data()
+    schedule_refresh(interval)
+    {:ok, %State{state | state: initial_state}}
+  end
+
+  def handle_info(:refresh, %State{refresh_interval: interval} = state) do
     IO.puts "Refreshing the cache...."
     new_state = run_tasks_to_get_sensor_data()
-    schedule_refresh(5)
-    {:noreply, new_state}
+    schedule_refresh(interval)
+    {:noreply, %State{state | state: new_state}}
   end
 
   def handle_info(unexpected, state) do
@@ -34,13 +43,19 @@ defmodule Servy.SensorServer do
     {:noreply, state}
   end
 
-  defp schedule_refresh(seconds) do
-    Process.send_after(self(), :refresh, :timer.seconds(seconds))
+  defp schedule_refresh(interval) do
+    Process.send_after(self(), :refresh, interval)
   end
 
-  def handle_call(:get_sensor_data, _from, state) do
-    {:reply, state, state}
+  def handle_call(:get_sensor_data, _from, %State{state: current_state} = state) do
+    {:reply, current_state, state}
   end
+
+  def handle_cast({:set_refresh_interval, interval}, state) do
+    IO.puts "Setting new interval.. #{interval}ms"
+    {:noreply, %State{state | refresh_interval: interval}}
+  end
+
 
   defp run_tasks_to_get_sensor_data do
     IO.puts "Running tasks to get sensor data..."
